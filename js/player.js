8a581987504879a224a33cf1eab271aca6c3b8b1
lduzhenlin <956459902@163.com>
2025-08-19 17:59:37 +0800

    修复密码错误

--- js/player.js
+++ js/player.js
@@ -96,20 +96,7 @@ Artplayer.FULLSCREEN_WEB_IN_BODY = true;
 
 // 页面加载
 document.addEventListener('DOMContentLoaded', function () {
-    // 先检查用户是否已通过密码验证
-    if (!isPasswordVerified()) {
-        // 隐藏加载提示
-        document.getElementById('player-loading').style.display = 'none';
-        return;
-    }
-
-    initializePageContent();
-});
-
-// 监听密码验证成功事件
-document.addEventListener('passwordVerified', () => {
-    document.getElementById('player-loading').style.display = 'block';
-
+    // 由于密码保护已禁用，直接初始化页面内容
     initializePageContent();
 });
 
